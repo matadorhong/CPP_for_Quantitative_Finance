0f8f702117c9854e26ad8d9b193121e5fcf3dab9
David Li <31947328+David-SF2290@users.noreply.github.com>
2018-06-23 21:44:28 -0400

    Update Changing Annual Payment.cpp

--- Loan/Changing Annual Payment.cpp
+++ Loan/Changing Annual Payment.cpp
@@ -1,6 +1,7 @@
-//** C++ For Quantitative Finance
-//** Loan Calculation (Changing Annual Payment)
-//** Author: David Li 
+
+/* C++ For Quantitative Finance                                 */
+/* Loan Calculation (Changing Annual Payment)                   */
+/* Author: David Li                                             */
 
 #include <iostream>
 #include <cstring>
@@ -23,147 +24,89 @@ int intC = 2; // column number for interest component
 int prinC = 3; // column number for principal component
 int endC = 4; // column number for period ending balance
 
-			for (int i = 1; i <= 5; i++)
-		{
-			cout << "Enter periodic interest rate in decimal: ";
-			cin >> intRate;
+for (int i = 1; i <= 5; i++) {
+     cout << "Enter periodic interest rate in decimal: ";
+     cin >> intRate;
 
-			if ((intRate < 0.0) || (intRate > 0.15))
-			{
-				if (i == 5)
-				{
-					cout << "\nThis program is being terminated\n";
-					system("PAUSE");
-					return 0;
-				}
-
-				else
-				{
-					cout << "You must enter a value between ";
-					cout << "0.0 and 0.15. Please try again." << endl;
-				}
-
-			}
-			else { break; }
-
-		}
+ if ((intRate < 0.0) || (intRate > 0.15)) {
+      if (i == 5) {
+      cout << "\nThis program is being terminated\n";
+      system("PAUSE");
+      return 0; } else {
+      cout << "You must enter a value between ";
+      cout << "0.0 and 0.15. Please try again." << endl; } }
+      else { break; } }
 		
 		
-					for (int i = 1; i <= 5; i++)
-		{
-			cout << "Enter periodic jumpSize1 in decimal: ";
-			cin >> jumpSize1;
+for (int i = 1; i <= 5; i++) {
+     cout << "Enter periodic jumpSize1 in decimal: ";
+     cin >> jumpSize1;
 
-			if ((jumpSize1 < 0.0) || (jumpSize1 > 0.3))
-			{
-				if (i == 5)
-				{
-					cout << "\nThis program is being terminated\n";
-					system("PAUSE");
-					return 0;
-				}
-
-				else
-				{
-					cout << "You must enter a value between ";
-					cout << "0.0 and 0.3. Please try again." << endl;
-				}
-
-			}
-			else { break; }
-
-		}
+     if ((jumpSize1 < 0.0) || (jumpSize1 > 0.3)) {
+     if (i == 5) {
+     cout << "\nThis program is being terminated\n";
+     system("PAUSE");
+     return 0; } else
+   {
+     cout << "You must enter a value between ";
+     cout << "0.0 and 0.3. Please try again." << endl; } }
+     else { break; }}
 		
 		
-						for (int i = 1; i <= 5; i++)
-		{
-			cout << "Enter periodic jumpSize2 in decimal: ";
-			cin >> jumpSize2;
+for (int i = 1; i <= 5; i++) {
+     cout << "Enter periodic jumpSize2 in decimal: ";
+     cin >> jumpSize2;
 
-			if ((jumpSize2 < 0.0) || (jumpSize2 > 0.3))
-			{
-				if (i == 5)
-				{
-					cout << "\nThis program is being terminated\n";
-					system("PAUSE");
-					return 0;
-				}
-
-				else
-				{
-					cout << "You must enter a value between ";
-					cout << "0.0 and 0.3. Please try again." << endl;
-				}
-
-			}
-			else { break; }
-
-		}	
+    if ((jumpSize2 < 0.0) || (jumpSize2 > 0.3)) {
+    if (i == 5) {
+    cout << "\nThis program is being terminated\n";
+    system("PAUSE");
+    return 0; } else
+    {
+    cout << "You must enter a value between ";
+    cout << "0.0 and 0.3. Please try again." << endl; }
+    } else { break; } }	
 		
 		
 		
-			for (int i = 1; i <= 5; i++)
-		{
-			cout << "Enter loanlife in periods: ";
-			cin >> loanLife;
+for (int i = 1; i <= 5; i++) {
+   cout << "Enter loanlife in periods: ";
+   cin >> loanLife;
 
-			if ((loanLife < 0) || (loanLife > 50))
-			{
-				if (i == 5)
-				{
-					cout << "\nThis program is being terminated\n";
-					system("PAUSE");
-					return 0;
-				}
-				else
-				{
-					cout << "You must enter a value between ";
-					cout << "0 and 50. Please try again." << endl;
-				}
-			}
-			else { break; }
-		}	
-		
-		
-		
+if ((loanLife < 0) || (loanLife > 50)) {
+   if (i == 5) {
+   cout << "\nThis program is being terminated\n";
+   system("PAUSE");
+   return 0; } else {
+   cout << "You must enter a value between ";
+   cout << "0 and 50. Please try again." << endl; } }
+   else { break; } }					
 				
-				for (int i = 1; i <= 5; i++)
-		{
-			cout << "Enter initial loan amount: ";
-			cin >> loanAmnt;
+for (int i = 1; i <= 5; i++) {
+     cout << "Enter initial loan amount: ";
+     cin >> loanAmnt;
 
-			if ((loanAmnt < 0.0) || (loanAmnt > 1000000.0))
-			{
-				if (i == 5)
-				{
-					cout << "\nThis program is being terminated\n";
-					system("PAUSE");
-					return 0;
-				}
-				else
-				{
-					cout << "You must enter a value between ";
-					cout << "0.0 and 1000000.0. Please try again." << endl;
-				}
-			}
-			else break;
-		}
+     if ((loanAmnt < 0.0) || (loanAmnt > 1000000.0)) {
+     if (i == 5) {
+     cout << "\nThis program is being terminated\n";
+     system("PAUSE");
+     return 0; } else {
+     cout << "You must enter a value between ";
+     cout << "0.0 and 1000000.0. Please try again." << endl; } }
+     else break; }
+	
+tolerance = 1.0;
 		
-		
-		 tolerance = 1.0;
-		
-		
-		
-	for(i=0; i<maxPrds; i++) for(j=0; j<5; j++) aT[i][j] = 0.;
+for(i=0; i<maxPrds; i++) for(j=0; j<5; j++) aT[i][j] = 0.;
     aT[loanLife -1][endC] = 1000.;
 
     stepSize = 0.; // initial value
     initPmnt = (loanAmnt*intRate)/(1+jumpSize1)/(1+jumpSize2);	
 		
 
-//*****************************************************************
-// Iteration to determine required periodic payment
-//*****************************************************************
+/*****************************************************************/
+/* Iteration to determine required periodic payment              */
+/*****************************************************************/
 for(iter = 1; aT[loanLife -1][endC] >= tolerance; iter++)
 {
 initPmnt += stepSize;
@@ -189,24 +132,23 @@ stepSize = aT[loanLife-1][endC]/pow((1 + intRate), loanLife)/loanLife /(1+jumpSi
 cout << iter << ". " << aT[loanLife-1][endC] << endl;
 }
 
-//*****************************************************************
-// Output
-//*****************************************************************
+/****************************************************************/
+/* Output                                                       */
+/****************************************************************/
 cout << endl;
 cout << "Number of iterations needed: " << iter - 1 << endl;
 cout << "Remaining balance: ";
 		
 
- cout << aT[loanLife-1][endC] << endl << endl;
+cout << aT[loanLife-1][endC] << endl << endl;
 
- printT(aT, loanLife);
- cout << endl <<endl;
+printT(aT, loanLife);
+cout << endl <<endl;
 
-//Wait for the user to read the output on the console
+/*    Wait for the user to read the output on the console    */ 
 system("PAUSE");
 return 0;
 }
-
 
 
 void printT(double aT[][5], int loanLife)
@@ -232,8 +174,7 @@ cout << endl << i+1;
 for(j = 0; j <5; j++)
 {
 cout.width(12);
-cout << aT[i][j];
-}
+cout << aT[i][j];}
 }
 return;
 }		
