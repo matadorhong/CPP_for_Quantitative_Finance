3e774d80fc7dd71f2ac8144ae4c23b046ff2e7c4
David Li <31947328+David-SF2290@users.noreply.github.com>
2018-06-23 22:29:32 -0400

    Update Changing Interest Rate.cpp

--- Loan/Changing Interest Rate.cpp
+++ Loan/Changing Interest Rate.cpp
@@ -1,15 +1,15 @@
 
-//** C++ For Quantitative Finance
-//** Loan Calculation (Changing Interest Rate)
-//** Author: David Li 
+/* C++ For Quantitative Finance                    */
+/* Loan Calculation (Changing Interest Rate)       */
+/* Author: David Li                                */
 
 #include <iostream>
 #include <cstring>
 #include <cmath>
 using namespace std;
 
-//int getInput(string msg, int min, int max);
-//double getInput(string msg, double min, double max);
+/* int getInput(string msg, int min, int max);         */
+/* double getInput(string msg, double min, double max);*/
 void printT(double aT[][5], int);
 
 int main()
@@ -28,191 +28,108 @@ int main()
 	int pmntC = 1; // column number for constant periodic payments
 	int intC = 2; // column number for interest component
 	int prinC = 3; // column number for principal component
-	int endC = 4; // column number for period ending balance
+	int endC = 4; // column number for period ending balance	
+	
+/******************************************************************/
+/*     Inputs:                                                    */
+/*   - intRate                                                    */
+/*   - loanLife                                                   */
+/*   - loanAmnt                                                   */
+/******************************************************************/
 	
 	
+for (int i = 1; i <= 5; i++) {
+     cout << "Enter periodic interest rate1 in decimal: ";
+     cin >> intRate1;
+
+   if ((intRate1 < 0.0) || (intRate1 > 0.15)) {
+     if (i == 5) {
+       cout << "\nThis program is being terminated\n";
+       system("PAUSE");
+       return 0; } else {
+       cout << "You must enter a value between ";
+       cout << "0.0 and 0.15. Please try again." << endl; }
+       } else { break; } }	
 	
-	//*******************************************************************
-	// Inputs:
-	//   - intRate
-	//   - loanLife
-	//   - loanAmnt
-	//*****************************************************************
+for (int i = 1; i <= 5; i++) {
+    cout << "Enter periodic interest rate2 in decimal: ";
+    cin >> intRate2;
+
+  if ((intRate2 < 0.0) || (intRate2 > 0.15)) {
+     if (i == 5) {
+       cout << "\nThis program is being terminated\n";
+       system("PAUSE");
+       return 0; } else {
+       cout << "You must enter a value between ";
+       cout << "0.0 and 0.15. Please try again." << endl; }}
+       else { break; }}		
+		
+for (int i = 1; i <= 5; i++) {
+     cout << "Enter periodic interest rate in decimal: ";
+     cin >> intRate3;
+
+     if ((intRate3 < 0.0) || (intRate3 > 0.15)) {
+        if (i == 5) {
+	  cout << "\nThis program is being terminated\n";
+          system("PAUSE");
+	  return 0; } else {
+	  cout << "You must enter a value between ";
+	  cout << "0.0 and 0.15. Please try again." << endl; } }
+	  else { break; } }		
+		
+for (int i = 1; i <= 5; i++) {
+     cout << "Enter loanlife1 in periods: ";
+     cin >> loanLife1;
+    
+     if ((loanLife1 < 0) || (loanLife1 > 50)) {
+        if (i == 5) {
+          cout << "\nThis program is being terminated\n";
+          system("PAUSE");
+          return 0; } else {
+	  cout << "You must enter a value between ";
+	  cout << "0 and 50. Please try again." << endl; }}
+	  else { break; }}		
+		
+for (int i = 1; i <= 5; i++) {
+     cout << "Enter loanlife2 in periods: ";
+     cin >> loanLife1;
 	
-	
-			for (int i = 1; i <= 5; i++)
-		{
-			cout << "Enter periodic interest rate1 in decimal: ";
-			cin >> intRate1;
-
-			if ((intRate1 < 0.0) || (intRate1 > 0.15))
-			{
-				if (i == 5)
-				{
-					cout << "\nThis program is being terminated\n";
-					system("PAUSE");
-					return 0;
-				}
-
-				else
-				{
-					cout << "You must enter a value between ";
-					cout << "0.0 and 0.15. Please try again." << endl;
-				}
-
-			}
-			else { break; }
-
-		}
-	
-	
-			for (int i = 1; i <= 5; i++)
-		{
-			cout << "Enter periodic interest rate2 in decimal: ";
-			cin >> intRate2;
-
-			if ((intRate2 < 0.0) || (intRate2 > 0.15))
-			{
-				if (i == 5)
-				{
-					cout << "\nThis program is being terminated\n";
-					system("PAUSE");
-					return 0;
-				}
-
-				else
-				{
-					cout << "You must enter a value between ";
-					cout << "0.0 and 0.15. Please try again." << endl;
-				}
-
-			}
-			else { break; }
-
-		}
+     if ((loanLife2 < 0) || (loanLife2 > 50)) {
+	if (i == 5) {
+	cout << "\nThis program is being terminated\n";
+	system("PAUSE");
+	return 0; } else {
+	cout << "You must enter a value between ";
+	cout << "0 and 50. Please try again." << endl; } }
+	else { break; } }		
 		
-		
-				for (int i = 1; i <= 5; i++)
-		{
-			cout << "Enter periodic interest rate in decimal: ";
-			cin >> intRate3;
+for (int i = 1; i <= 5; i++) {
+     cout << "Enter loanlife3 in periods: ";
+     cin >> loanLife1;
 
-			if ((intRate3 < 0.0) || (intRate3 > 0.15))
-			{
-				if (i == 5)
-				{
-					cout << "\nThis program is being terminated\n";
-					system("PAUSE");
-					return 0;
-				}
+   if ((loanLife3 < 0) || (loanLife3 > 50)) {
+      if (i == 5) {
+	 cout << "\nThis program is being terminated\n";
+	 system("PAUSE");
+	 return 0; } else {
+	 cout << "You must enter a value between ";
+	 cout << "0 and 50. Please try again." << endl; }}
+	 else { break; }}	
+		
+for (int i = 1; i <= 5; i++) {
+     cout << "Enter initial loan amount: ";
+     cin >> loanAmnt;
 
-				else
-				{
-					cout << "You must enter a value between ";
-					cout << "0.0 and 0.15. Please try again." << endl;
-				}
-
-			}
-			else { break; }
-
-		}
+   if ((loanAmnt < 0.0) || (loanAmnt > 1000000.0)) {
+      if (i == 5) {
+	 cout << "\nThis program is being terminated\n";
+	 system("PAUSE");
+	 return 0; } else {
+	 cout << "You must enter a value between ";
+	 cout << "0.0 and 1000000.0. Please try again." << endl; } }
+	 else break; }
 		
-		
-		for (int i = 1; i <= 5; i++)
-		{
-			cout << "Enter loanlife1 in periods: ";
-			cin >> loanLife1;
-
-			if ((loanLife1 < 0) || (loanLife1 > 50))
-			{
-				if (i == 5)
-				{
-					cout << "\nThis program is being terminated\n";
-					system("PAUSE");
-					return 0;
-				}
-				else
-				{
-					cout << "You must enter a value between ";
-					cout << "0 and 50. Please try again." << endl;
-				}
-			}
-			else { break; }
-		}
-		
-		
-		for (int i = 1; i <= 5; i++)
-		{
-			cout << "Enter loanlife2 in periods: ";
-			cin >> loanLife1;
-
-			if ((loanLife2 < 0) || (loanLife2 > 50))
-			{
-				if (i == 5)
-				{
-					cout << "\nThis program is being terminated\n";
-					system("PAUSE");
-					return 0;
-				}
-				else
-				{
-					cout << "You must enter a value between ";
-					cout << "0 and 50. Please try again." << endl;
-				}
-			}
-			else { break; }
-		}
-		
-		
-		for (int i = 1; i <= 5; i++)
-		{
-			cout << "Enter loanlife3 in periods: ";
-			cin >> loanLife1;
-
-			if ((loanLife3 < 0) || (loanLife3 > 50))
-			{
-				if (i == 5)
-				{
-					cout << "\nThis program is being terminated\n";
-					system("PAUSE");
-					return 0;
-				}
-				else
-				{
-					cout << "You must enter a value between ";
-					cout << "0 and 50. Please try again." << endl;
-				}
-			}
-			else { break; }
-		}
-		
-		
-				for (int i = 1; i <= 5; i++)
-		{
-			cout << "Enter initial loan amount: ";
-			cin >> loanAmnt;
-
-			if ((loanAmnt < 0.0) || (loanAmnt > 1000000.0))
-			{
-				if (i == 5)
-				{
-					cout << "\nThis program is being terminated\n";
-					system("PAUSE");
-					return 0;
-				}
-				else
-				{
-					cout << "You must enter a value between ";
-					cout << "0.0 and 1000000.0. Please try again." << endl;
-				}
-			}
-			else break;
-		}
-		
-		
-		
-		 tolerance = 1.0;
- 
+tolerance = 1.0;
  
 loanLife =loanLife1 + loanLife2 +loanLife3;
 
@@ -228,15 +145,12 @@ if (intRate2 > maxInt) maxInt = intRate2;
 if (intRate3 < maxInt) maxInt = intRate3;
 
 stepSize = 0.; // initial value
-prdPmnt = loanAmnt*minInt; //initial guess for payment
+prdPmnt = loanAmnt*minInt; //initial guess for payment	
 		
 		
-		
-		
-		
-//*****************************************************************
-// Iteration to determine required periodic payment
-//*****************************************************************
+/*****************************************************************/
+/* Iteration to determine required periodic payment              */
+/*****************************************************************/
 for(iter = 1; aT[loanLife -1][endC] >= tolerance; iter++)
 {
 prdPmnt += stepSize;
@@ -262,9 +176,9 @@ cout << iter << ". " << aT[loanLife-1][endC] << endl;
 }
 
 
-//*****************************************************************
-// Output
-//*****************************************************************
+/*****************************************************************/
+/* Output                                                        */
+/*****************************************************************/
 cout << endl;
 cout << "Required periodic payment: " << prdPmnt << endl;
 cout << "Number of iterations needed: " << iter - 1 << endl;
@@ -274,7 +188,7 @@ cout << aT[loanLife-1][endC] << endl << endl;
 printT(aT, loanLife);
 cout << endl << endl;
 
-//Wait for the user to read the output on the console
+/*     Wait for the user to read the output on the console      */ 
 system("PAUSE");
 return 0;
 }
